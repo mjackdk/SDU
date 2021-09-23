02629252b6dbd5994de2f4befd8bbbcfd8e07e82
Michael Jack <50518949+mjackdk@users.noreply.github.com>
2021-09-23 14:57:19 +0200

    Comments and clean-up

--- HelloWorld/src/main.c
+++ HelloWorld/src/main.c
@@ -1,14 +1,16 @@
-#define F_CPU 16000000UL
+#define F_CPU 16000000UL // 16 MHZ
 
-#include <stdio.h>
-#include <avr/io.h>
-#include <util/delay.h>
+// libraries from avr-libc, the AVR C runtime library
+#include <avr/io.h> // device-specific IO definitions
+#include <stdio.h> // standard IO facilities. printf etc.
+#include <util/delay.h> // basic busy-wait delay loops, _delay_ms etc.
 
-#include "usart.h"
+// local libraries
+#include "usart.h" // scanf etc.
 
 int main(void) {
-    uart_init(); // open the communication to the microcontroller
-    io_redirect(); // redirect input and output to the communication
+    uart_init(); // initialize uart
+    io_redirect(); // redirect input and output to host computer
 
     while(1){
         _delay_ms(1000);
