e4cfec12eb9240f70d96842183fcb5f934cdf92d
Michael Jack <50518949+mjackdk@users.noreply.github.com>
2021-09-23 13:20:24 +0200

    Basic Hello World example

---
+++ HelloWorld/src/main.c
@@ -0,0 +1,32 @@
+/*
+ * HelloWorld.c
+ *
+ * Created: 9/13/2021 10:43:27 AM
+ * Author : Alin
+ */ 
+
+
+ 
+#define F_CPU 16000000UL
+
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include "usart.h"
+
+int main(void) {    
+
+    uart_init(); // open the communication to the microcontroller
+	io_redirect(); // redirect input and output to the communication
+    
+    while(1) {
+		
+		_delay_ms(1000)	;
+		
+        printf("Hello World!!! \n");
+       
+    }
+        
+    return 0;
+}
