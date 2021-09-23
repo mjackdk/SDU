35940e0c33bf2ac5adf8b6209f9919e6640592fd
Michael Jack <50518949+mjackdk@users.noreply.github.com>
2021-09-23 14:06:15 +0200

    Minor tweaks

--- HelloWorld/src/main.c
+++ HelloWorld/src/main.c
@@ -7,18 +7,12 @@
 #include "usart.h"
 
 int main(void) {
-
     uart_init(); // open the communication to the microcontroller
     io_redirect(); // redirect input and output to the communication
 
-    while(1) {
-
-        _delay_ms(1000)	;
-		
-        printf("Hello World!!! \n");
-
+    while(1){
+        _delay_ms(1000);
+        printf("Hello World!\n");
     }
-
     return 0;
-    
 }
