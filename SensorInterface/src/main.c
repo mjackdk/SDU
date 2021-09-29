09d591a7d5531898fb1b44fd922bc9fc3797e8b7
Michael Jack <50518949+mjackdk@users.noreply.github.com>
2021-09-29 11:10:54 +0200

    Lab exercise

--- SensorInterface/src/main.c
+++ SensorInterface/src/main.c
@@ -2,14 +2,20 @@
 #include <stdio.h>
 #include <util/delay.h>
 
+#include "usart.h" // scanf etc.
+
 #define ADC_PIN 0 // ADC channel
 
 // Function declaration
 uint16_t adc_read(uint8_t adc_channel);
 
 int main(){
+    uart_init(); // initialize uart
+    io_redirect(); // redirect input and output to host computer
 
     uint16_t adc_result;
+    float voltage;
+    float temperature;
 
     // Set Vref = AVcc
     ADMUX = (1<<REFS0);
@@ -18,10 +24,16 @@ int main(){
     ADCSRA = (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
 
     while(1){
-
+        _delay_ms(1000);
         adc_result = adc_read(ADC_PIN);
+        printf("ADC reading: %d\n", adc_result);
 
-        // do something...
+        voltage = ((5.077/1024.0) * adc_result) - 1.0;
+        printf("Voltage: %f\n", voltage);
+
+        temperature = 100.0 * voltage;
+        printf("Temperature: %f\n\n", temperature);
+
     }   
 }
 
