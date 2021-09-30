cacce7ec072ea6bf3660c6b2ed1f2492d64dd7d0
Michael Jack <50518949+mjackdk@users.noreply.github.com>
2021-09-30 10:51:21 +0200

    Initial project

---
+++ GuessingGame/src/main.c
@@ -0,0 +1,15 @@
+// libraries from avr-libc, the AVR C runtime library
+#include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>
+
+// local libraries
+#include "usart.h"
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
