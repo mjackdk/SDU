dde0972d74aa52f5c77fe0c00ee0db9ee569ec81
Michael Jack <50518949+mjackdk@users.noreply.github.com>
2021-11-26 06:14:01 +0100

    Final version

--- IOBoardTest/src/main.c
+++ IOBoardTest/src/main.c
@@ -10,16 +10,31 @@
 #include "ds1621.h" // Temperature sensor
 
 int main (void){
+    DDRC = 0xF0; // IO board PC0...3 as inputs, for buttons
+    DDRD = 0xFF; // IO board PD4...7 as outputs, for LEDs
+    PORTC = 0x3F; // enable internal pullups for PC0...3
+    PORTD = 0x00; // set output LEDs to off
     i2c_init(); // initialize I2C communication
     ds1621_init(); // initialize temperature sensor
     LCD_init(); //initialize the LCD
 
-    double temperature;
+    // double temperature;
 
     while (1){
-        temperature = get_temperature();
+        PORTC = 0b00111111;
+        LCD_set_cursor(2,1);
+        if (PINC == 0b00111110) { // is PC0 (DI1) connected to ground?
+            printf("DI0");
+            PORTD = 0b00010000; // then turn on PD4 (LED1)
+        }
+        _delay_ms(500);
+
+
+/*      temperature = get_temperature();
         _delay_ms(500);
         LCD_set_cursor(1,1);
         printf("Temp = %.2f\337C   ", temperature);
+ */
+
     }
 }
